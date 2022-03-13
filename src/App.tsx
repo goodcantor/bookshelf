34d8a3ece1a552ab63b1bc3c17593131d5363d94
goodcantor <goodcantor@mail.ru>
2022-03-13 17:21:07 +0300

    3 default cards

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,35 @@ import { PopupFilter } from "./components/Popup";
 import { ModalButton } from "./components/Modal";
 
 export const App = () => {
-  const [books, setBooks] = React.useState([]);
+  const [books, setBooks] = React.useState([
+    {
+      title:
+        "EFFECTIVE TYPESCRIPT: 62 SPECIFIC WAYS TO IMPROVE YOUR TYPESCRIPT",
+      subtitle:
+        "TypeScript is a typed superset of JavaScript with the potential to solve many of the headaches for which JavaScript is f headaches for which JavaScript is f headaches for which JavaScript is f",
+      author: "Apeka Dan",
+      img: "https://images-na.ssl-images-amazon.com/images/I/51FOcrxnzbL._SX379_BO1,204,203,200_.jpg",
+      rating: 4,
+    },
+    {
+      img: "https://eloquentjavascript.net/img/cover.jpg",
+      title:
+        "EFFECTIVE TYPESCRIPT: 62 SPECIFIC WAYS TO IMPROVE YOUR TYPESCRIPT",
+      subtitle:
+        "TypeScript is a typed superset of JavaScript with the potential to solve many of the headaches for which JavaScript is f...",
+      author: "Apeka Dan",
+      rating: 4,
+    },
+    {
+      img: "https://res.cloudinary.com/practicaldev/image/fetch/s--0TzY1R9i--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/4ovaa9po3t4ii8c4qlqr.png",
+      title:
+        "EFFECTIVE TYPESCRIPT: 62 SPECIFIC WAYS TO IMPROVE YOUR TYPESCRIPT",
+      subtitle:
+        "TypeScript is a typed superset of JavaScript with the potential to solve many of the headaches for which JavaScript is f...",
+      author: "Apeka Dan",
+      rating: 4,
+    },
+  ]);
 
   const handleSubmit = (book) => {
     // @ts-ignore
